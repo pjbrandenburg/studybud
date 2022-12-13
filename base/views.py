b56c39e18a3217329122df5ac024a13a0d78aeb4
P.Brandenburg <pbrandenburg@agmobilerobots.com>
2022-12-13 10:01:43 -0500

    Added Templates

--- base/views.py
+++ base/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {"id":1, "name":"Let's learn Django!"},
+    {"id":2, "name":"Let's learn Fullstack!"},
+    {"id":3, "name":"Let's learn Backend!"},
+]
 
 def home(request):
-    return HttpResponse("Home Page")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
-def room(request):
-    return HttpResponse("Room")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {"room": room}
+
+    return render(request, "base/room.html", context)
