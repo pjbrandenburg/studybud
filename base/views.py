c1cbdb43abc776c56596b24e99d7207721190d96
P.Brandenburg <pbrandenburg@agmobilerobots.com>
2022-12-14 12:13:52 -0500

    Added user authentication and flash messages

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -10,6 +13,32 @@ from .forms import RoomForm
 #     {"id":2, "name":"Let's learn Fullstack!"},
 #     {"id":3, "name":"Let's learn Backend!"},
 # ]
+
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User Does Not Exist!")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username OR Password Does Not Exist!")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ""
