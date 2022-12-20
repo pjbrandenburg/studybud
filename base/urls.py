2cc1784b4862840f8899b3f8a383c37af40afea4
P.Brandenburg <pbrandenburg@agmobilerobots.com>
2022-12-20 16:29:19 -0500

    Added topics and activities pages, mobile responsiveness

--- base/urls.py
+++ base/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     path("delete-message/<str:pk>", views.deleteMessage, name="delete-message"),
     path("update-user/", views.updateUser, name="update-user"),
 
+    path("topics/", views.topicsPage, name="topics"),
+    path("activities/", views.activityPage, name="activities"),
+
 ]
